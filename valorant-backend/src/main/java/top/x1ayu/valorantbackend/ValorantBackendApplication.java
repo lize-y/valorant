86db1c89a1bd8e1c745348f8c7e18b59f18a7351
x1ayu <13377540435@163.com>
2025-05-18 16:06:50 +0800

    init project

---
+++ valorant-backend/src/main/java/top/x1ayu/valorantbackend/ValorantBackendApplication.java
@@ -0,0 +1,13 @@
+package top.x1ayu.valorantbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorantBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValorantBackendApplication.class, args);
+    }
+
+}
---
+++ valorant-backend/src/test/java/top/x1ayu/valorantbackend/ValorantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package top.x1ayu.valorantbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValorantBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
